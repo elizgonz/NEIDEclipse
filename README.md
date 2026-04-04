49bab756f36b658a579f7d72deef2aec0718ae7a
elizgonz <64707754+elizgonz@users.noreply.github.com>
2026-04-04 11:42:18 -0400

    sample repo

---
+++ README.md
@@ -0,0 +1,80 @@
+# Shadowing the Sun: Testing Synthetic Granulation Spectra with NEID Solar Eclipse Observations (Gonzalez et al. 2026)
+
+GRASS (GRanulation And Spectrum Simulator) is a Julia package designed to produce time series of stellar spectra with realistic line-shape changes from solar granulation. GRASS v1.0.x is described in detail in [Palumbo et al. (2022)](https://arxiv.org/abs/2110.11839) and GRASS v2.0.x is presented in [Palumbo et al. (2024a)](https://arxiv.org/abs/2405.07945).
+
+GRASS-E repurposes the GRASS software to produce granulation-driven solar spectra during a solar eclipse. GRASS-E is presented and validated against the 2023 October 14 solar eclipse in Gonzalez et al. 2026. This repository provides the scripts and required information to generate the models (via GPU) from Gonzalez et al. 2026. 
+
+## Installation
+
+GRASS is written entirely in Julia and requires Julia v1.9 or greater. Installation instructions for Julia are available from [julialang.org](https://julialang.org/downloads/).
+
+GRASS itself only requires a few steps to install. Simply clone the repo to your desired directory...
+
+```bash
+git clone git@github.com:palumbom/GRASS.git
+cd GRASS 
+julia
+```
+
+... and then add it with Julia's built-in package manager, `Pkg`:
+
+```julia
+using Pkg
+Pkg.add(path=".") # assuming you are in /PATH/TO/GRASS
+using GRASS
+```
+
+If you wish to develop or otherwise contribute to GRASS, instead add the package in develop mode:
+
+```julia
+using Pkg
+Pkg.develop(path=".") # assuming you are in /PATH/TO/GRASS
+using GRASS
+```
+
+Upon first invocation of GRASS, Julia will automatically install the package dependencies and download the required input data. The input data can be re-installed by invoking
+
+```julia
+Pkg.build("GRASS")
+```
+
+## Basic GRASS-E CPU Example
+
+```julia
+using GRASS
+using PyPlot
+using DataFrames
+using CSV
+
+#NEID solar feed coordinates
+obs_lat = 31.9583 
+obs_long = -111.5967  
+alt = 2.097938
+
+# parameters for lines in the spectra
+lines = [5434.5232]                 # array of line centers 
+templates = ["FeI_5434"]            # template data to use
+depths = [0.6]                      # array of line depths
+variability = trues(length(lines))  # granulation signature toggle 
+blueshifts = zeros(length(lines))   # set convective blueshift value
+resolution = 7e5                    # spectral resolution
+spec = GRASS.SpecParams(lines=lines, depths=depths, variability=variability, blueshifts=blueshifts, templates=templates, resolution=resolution) 
+
+# read in NEID timestamps for October solar eclipse 
+df = CSV.read("data/NEIDOctoberTimestamps.csv", DataFrame)
+# specify number of epochs 
+disk = GRASS.Eclipse.DiskParamsEclipse(Nt=length(df.UTC))
+
+# specify limb darkening law to use
+LD_type = "SSD_4parameter"
+# specify optimized wavelength-dependent extinction coefficent  
+ext_coeff = 0.15452995224327976
+
+# synthesize the spectra
+wavelengths, flux = GRASS.Eclipse.synthesize_spectra_eclipse(spec, disk, lines, LD_type, obs_long, obs_lat, alt, string.(df.UTC), ext_coeff, ext_toggle=true, use_gpu=false)
+# compute radial velocities
+wavs_sim, flux_sim = GRASS.convolve_gauss(wavelengths, flux, new_res=11e4)
+v_grid_cpu, ccf_cpu = GRASS.calc_ccf(wavs_sim, flux_sim, lines, depths, 11e4)
+rv_cpu, rv_error = GRASS.calc_rvs_from_ccf(v_grid_cpu, ccf_cpu)
+```
+![RM](figures/sample_eclipse_plot.png)
---
+++ figures/RM/CB/make_plots.py
@@ -0,0 +1,87 @@
+import h5py
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+mpl = plt.matplotlib 
+import matplotlib.dates as mdates
+from datetime import datetime, timedelta
+from astropy.time import Time
+from barycorrpy import get_BC_vel
+import matplotlib.lines as mlines
+
+#these projected models are for N=197, sub=40x40, with extinction, with sunspots 
+    # with CB optimized polynomial from projected RVs (model 2 has GRASS CB)
+    # (flux & CB effect), no MF
+projected_SSD_4parameter = h5py.File("data/projected_SSD_4parameter_gpu_ext_CB_optim.jld2", "r")
+projected_RV_SSD_4parameter = projected_SSD_4parameter["RV_list_no_cb"][()]
+
+grass_on_SSD_4parameter = h5py.File("data/neid_all_lines_rv_on_SSD_4parameter_gpu_ext.jld2", "r")
+grass_on_RV_SSD_4parameter = grass_on_SSD_4parameter["rv_asymmetric_lsf"][()]
+
+grass_off_SSD_4parameter = h5py.File("data/neid_all_lines_rv_off_SSD_4parameter_gpu_ext_CB_optim.jld2", "r")
+grass_off_RV_SSD_4parameter = grass_off_SSD_4parameter["rv_asymmetric_lsf"][()]
+
+data = pd.read_csv("../../../data/NEID_Data.csv")
+UTC_time = []
+time_julian = []
+for i in data["obsdate"][15:-150]:
+    dt = datetime.strptime(i, "%Y-%m-%d %H:%M:%S") + timedelta(seconds=27.5)
+    UTC_time.append(dt)
+    time_julian.append((Time(dt)).jd)
+
+line_data = h5py.File("../../../data/neid_RVlinebyline.jld2", "r")
+line_lines = line_data["name"][()]
+line_rv  = line_data["rv"][()]
+
+vb, warnings, flag = get_BC_vel(JDUTC=time_julian[0:-28], lat=31.9583 , longi=-111.5967, alt=209.7938, SolSystemTarget='Sun', predictive=False,zmeas=0.0)
+UTC_time = UTC_time[0:-28]
+
+def jld2_read(jld2_file, variable, vb, index):
+    array = jld2_file[variable[index]][()]
+    array = np.array(array + vb)
+    array -= array[-1]
+    return array
+
+line_list = ["FeI_5250.2","FeI_5250.5","FeI_5379","TiII_5381","FeI_5382","FeI_5383","MnI_5432","FeI_5432","FeI_5434","NiI_5435","FeI_5436.3","FeI_5436.6","FeI_5576","NiI_5578","FeII_6149","FeI_6151","CaI_6169.0","CaI_6169.5","FeI_6170","FeI_6173","FeI_6301","FeI_6302"]
+
+def bin_array(arr, bin_size):
+    arr = np.array(arr)
+    n_bins = len(arr) // bin_size
+    return np.array([np.nanmean(arr[i*bin_size:(i+1)*bin_size]) for i in range(n_bins)])
+
+for i in range(0,len(line_list)):    
+    line_rv_array = jld2_read(line_data, line_rv, vb, i)
+    projected_RV_SSD_4parameter_array = jld2_read(projected_SSD_4parameter, projected_RV_SSD_4parameter, vb, i)
+    grass_on_SSD_4parameter_array = jld2_read(grass_on_SSD_4parameter, grass_on_RV_SSD_4parameter, vb, i)
+    grass_off_SSD_4parameter_array = jld2_read(grass_off_SSD_4parameter, grass_off_RV_SSD_4parameter, vb, i)
+
+    fig, axs = plt.subplots(2, figsize=(7,8), sharex=True, sharey=False, gridspec_kw={'hspace': 0, 'height_ratios': [3, 1]})
+    axs[0].scatter(UTC_time, line_rv_array, color = 'k', marker = "x", s = 25)#, label = "NEID {} $\AA$ RVs".format(line_list[i])) 
+    
+    
+    rms1 = (round(np.sqrt((np.nansum((bin_array(line_rv_array,4) - bin_array(projected_RV_SSD_4parameter_array,4))**2))/len(bin_array(line_rv_array,4))),2))
+    rms2 = (round(np.sqrt((np.nansum((bin_array(line_rv_array,4) - bin_array(grass_off_SSD_4parameter_array,4))**2))/len(bin_array(line_rv_array,4))),2))
+    rms3 = (round(np.sqrt((np.nansum((bin_array(line_rv_array,4) - bin_array(grass_on_SSD_4parameter_array,4))**2))/len(bin_array(line_rv_array,4))),2))
+
+    axs[0].plot(UTC_time, projected_RV_SSD_4parameter_array, color = 'b', linestyle = "--", linewidth = 2, label = "Model I-CB: {} m/s".format(rms1))
+    axs[0].plot(UTC_time, grass_off_SSD_4parameter_array, color = 'y', linestyle = "--", linewidth = 2, label = "Model II-CB: {} m/s".format(rms2))
+    axs[0].plot(UTC_time, grass_on_SSD_4parameter_array, color = 'r', linestyle = "--", linewidth = 2, label = "Model III: {} m/s".format(rms3))
+
+    axs[0].xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
+    axs[0].set_xlabel("Time (UTC)", fontsize=15)
+    axs[0].set_ylabel("RV (m/s)", fontsize=15)
+    axs[0].legend(fontsize=11, frameon=False, loc="lower right")
+    axs[0].tick_params(axis='y', labelsize=11)
+
+    # residuals 
+    axs[1].scatter(UTC_time, line_rv_array - projected_RV_SSD_4parameter_array, color = 'b', marker = "x", s = 8) 
+    axs[1].scatter(UTC_time, line_rv_array - grass_off_SSD_4parameter_array, color = 'y', marker = "x", s = 8)  
+    axs[1].scatter(UTC_time, line_rv_array - grass_on_SSD_4parameter_array, color = 'r', marker = "x", s = 8) 
+    axs[1].xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
+    axs[1].set_xlabel("Time (UTC)", fontsize=15)
+    axs[1].set_ylabel("Residuals", fontsize=15) 
+    plt.xticks(fontsize=11)
+    plt.yticks(fontsize=11)
+    plt.tick_params(axis='x', which='both', top=True, labeltop=False)
+    plt.savefig("{}.png".format(line_list[i]))
+    plt.clf()
