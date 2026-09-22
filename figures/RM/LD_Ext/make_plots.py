correct normalization & earth velocity weight

figures/RM/LD_Ext/make_plots.py
@@ -37,7 +37,7 @@ UTC_time = UTC_time[0:-28]
 def jld2_read(jld2_file, variable, vb, index):
     array = jld2_file[variable[index]][()]
     array = np.array(array + vb)
-    array -= array[-1]
+    array -= np.nanmean(array)
     return array
 
 line_list = ["FeI_5250.2","FeI_5250.5","FeI_5379","TiII_5381","FeI_5382","FeI_5383","MnI_5432","FeI_5432","FeI_5434","NiI_5435","FeI_5436.3","FeI_5436.6","FeI_5576","NiI_5578","FeII_6149","FeI_6151","CaI_6169.0","CaI_6169.5","FeI_6170","FeI_6173","FeI_6301","FeI_6302"]